9841735dbe6854306f4707464fa6f674fbf54dba
renanflow <renanlopes.ads@gmail.com>
2021-12-20 22:25:41 -0300

    estrutura inicial

---
+++ src/AppMvcCompleta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppMvcCompleta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
