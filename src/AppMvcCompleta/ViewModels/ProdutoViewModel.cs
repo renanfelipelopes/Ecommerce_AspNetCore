d3dfa2a015891957df0b1df962d0b655a8275638
renanflow <renanlopes.ads@gmail.com>
2021-12-27 21:23:31 -0300

    ProdutoViewModel nova entidade

--- src/AppMvcCompleta/ViewModels/ProdutoViewModel.cs
+++ src/AppMvcCompleta/ViewModels/ProdutoViewModel.cs
@@ -15,6 +15,10 @@ namespace DevIO.App.ViewModels
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Fornecedor")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
 
