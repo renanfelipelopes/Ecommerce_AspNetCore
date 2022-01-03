c1c96a86a634d80cb872e4ece86b4ec9c5f4a82a
renanflow <renanlopes.ads@gmail.com>
2022-01-03 09:56:03 -0300

    MoedaAttribute validação

--- src/AppMvcCompleta/Extensions/MoedaAttribute.cs
+++ src/AppMvcCompleta/Extensions/MoedaAttribute.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace DevIO.App.Extensions
 {
     public class MoedaAttribute : ValidationAttribute
     {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                var moeda = Convert.ToDecimal(value, new CultureInfo("pt-BR"));
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(errorMessage: "Moeda em formato inválido");
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
