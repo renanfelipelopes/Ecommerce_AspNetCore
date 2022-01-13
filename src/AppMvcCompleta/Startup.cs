2275a0cec9402367b0a920ec686958ffdf2cd537
renanflow <renanlopes.ads@gmail.com>
2022-01-13 19:09:28 -0300

    AddIdentityConfiguration

--- src/AppMvcCompleta/Startup.cs
+++ src/AppMvcCompleta/Startup.cs
@@ -34,14 +34,11 @@ namespace AppMvcCompleta
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentityConfiguration(Configuration);
 
             services.AddDbContext<MeuDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
 
