9a5654950cd159839bf0e9747150abe3658c26ec
renanflow <renanlopes.ads@gmail.com>
2022-01-15 18:04:06 -0300

    FluentValidation

---
+++ src/DevIO.Business/Models/Validations/FornecedorValidation.cs
@@ -0,0 +1,22 @@
+﻿using AppMvcBasica.Models;
+using FluentValidation;
+
+namespace DevIO.Business.Models.Validations
+{
+    class FornecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(f => f.Nome)
+                .NotEmpty().WithMessage("o campo {PropertyName} precisa ser fornecido")
+                .Length(2, 100).WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres");
+
+            When(f => f.TipoFornecedor == TipoFornecedor.PessoaFisica, action: () =>
+            {
+                //RuleFor(expression: f => f.Documento.Length)
+            });
+
+            When(f => f.TipoFornecedor == TipoFornecedor.PessoaJuridica, action: () => { });
+        }
+    }
+}
