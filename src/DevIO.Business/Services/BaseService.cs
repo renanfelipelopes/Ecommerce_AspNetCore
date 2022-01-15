4c6900f0f4c9d99abe0a8f5d03b613b58e932f8a
renanflow <renanlopes.ads@gmail.com>
2022-01-15 17:03:57 -0300

    Base Service

---
+++ src/DevIO.Business/Services/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using DevIO.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Business.Services
+{
+    public abstract class BaseService
+    {
+    }
+
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+    }
+
+    public class ProdutoService : BaseService, IProdutoService
+    {
+    }
+}
