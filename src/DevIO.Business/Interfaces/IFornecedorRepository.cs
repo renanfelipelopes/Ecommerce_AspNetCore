e3fdeaa3bd4080b03f39dadefeb77daf4405f412
renanflow <renanlopes.ads@gmail.com>
2022-01-17 21:30:32 -0300

    Comentario

--- src/DevIO.Business/Interfaces/IFornecedorRepository.cs
+++ src/DevIO.Business/Interfaces/IFornecedorRepository.cs
@@ -6,9 +6,7 @@ namespace DevIO.Business.Interfaces
 {
     public interface IFornecedorRepository : IRepository<Fornecedor>
     {
-        //eu preciso colocar algum metodo aqui? nao, por que todos os metodos que eu defini já estao em Fornecedor, entao essa interface só extende especializando para a classe fornecedor
-        //porem, algumas coisas serao necessarias
-        Task<Fornecedor> ObterFornecedorEndereco(Guid id); //retornar um fornecedor, e tambem no msm metodo, obter o endereco junto
-        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id); //sera retornado tanto fornecedor, quanto a lista de produtos e o endereco do fornecedor
+        Task<Fornecedor> ObterFornecedorEndereco(Guid id); 
+        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id); 
     }
 }
