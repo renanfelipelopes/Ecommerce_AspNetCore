bfe1e21e22d74ca3a3b40ca3a4591491c722974c
renanflow <renanlopes.ads@gmail.com>
2022-01-21 14:54:34 -0300

    Adicionar FornecedorService

--- src/DevIO.Business/Services/FornecedorService.cs
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -8,11 +8,29 @@ namespace DevIO.Business.Services
 {
     public class FornecedorService : BaseService, IFornecedorService
     {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IEnderecoRepository _enderecoRepository;
+
+        public FornecedorService(IFornecedorRepository fornecedorRepository,
+                                 IEnderecoRepository enderecoRepository)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _enderecoRepository = enderecoRepository;
+        }
+
         public async Task Adicionar(Fornecedor fornecedor)
         {
-            // Validar o estado da entidade!
+            // Esse return fica aqui para por que embaixo vou fazer mais alguma coisa, entao ele serve para respeitar o fluxo do processo que salvaria no banco
             if (!ExecutarValidacao(new FornecedorValidation(), fornecedor)
-                && !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+                || !ExecutarValidacao(new EnderecoValidation(), fornecedor.Endereco)) return;
+
+            if (_fornecedorRepository.Buscar(f => f.Documento == fornecedor.Documento).Result.Any())
+            {
+                Notificar("Já existe um fornecedor com este documento infomado.");
+                return;
+            }
+
+            await _fornecedorRepository.Adicionar(fornecedor);
         }
 
         public async Task Atualizar(Fornecedor fornecedor)
