bb309dfca4417a4abbc96224c81856da5d4b8c52
renanflow <renanlopes.ads@gmail.com>
2022-01-21 21:50:38 -0300

    Comentarios

--- src/DevIO.Business/Notificacoes/Notificacao.cs
+++ src/DevIO.Business/Notificacoes/Notificacao.cs
@@ -5,14 +5,11 @@ namespace DevIO.Business.Notificacoes
 {
     public class Notificacao
     {
-        // O construtor vai receber a mensagem em si, o que quer dizer que eu sou obrigado a passar a mensagem via construtor
         public Notificacao(string mensagem)
         {
             Mensagem = mensagem;
         }
 
-        // Já eu só passo a mensagem via construtor, eu posso apenas passar uma entidade readonly sem o set, uma vez que eu
-        // já criei a instancia eu não preciso mais mexer nela
         public string Mensagem { get; set; }
     }
 }
--- src/DevIO.Business/Notificacoes/Notificador.cs
+++ src/DevIO.Business/Notificacoes/Notificador.cs
@@ -6,33 +6,23 @@ namespace DevIO.Business.Notificacoes
 {
     public class Notificador : INotificador
     {
-        // Essa lista vai ser uma lista global que vai durar todo o request, então como igual uma pilha,
-        // eu vou colocando notificações aqui dentro durante todo o processo de validação
-        // A noticacao vai ser lançada e manipulada pelo Handle
-        // Essa propriedade não pode ser readonly senão eu não vou poder adicionar as notificações 
         private List<Notificacao> _notificacoes;
 
-        // Uma vez que eu der um new no Notificador(), eu vou criar essa lista de notificações e pronto
-        // já posso começar a trabalhar com essa classe
         public Notificador()
         {
             _notificacoes = new List<Notificacao>();
         }
 
-        // O Handle vai pegar a notificação e adicionar ela na lista
         public void Handle(Notificacao notificacao)
         {
             _notificacoes.Add(notificacao);
         }
 
-        // Aqui só vamos retornar essa lista
         public List<Notificacao> ObterNotificacoes()
         {
             return _notificacoes;
         }
 
-        // Aqui só vamos dizer se tem ou não. Se eu tiver uma notificação significa existe um problema já notificado, 
-        // Então é o momento de tomar uma atitude
         public bool TemNotificacao()
         {
             return _notificacoes.Any();
