0307080e6aab35fc78d8e37b245756fbb15d9da2
renanflow <renanlopes.ads@gmail.com>
2022-01-24 22:01:16 -0300

    BaseController

--- src/AppMvcCompleta/Controllers/BaseController.cs
+++ src/AppMvcCompleta/Controllers/BaseController.cs
@@ -5,16 +5,16 @@ namespace DevIO.App.Controllers
 {
     public abstract class BaseController : Controller
     {
-        private readonly INotificador _notificador;
+        //private readonly INotificador _notificador;
 
-        protected BaseController(INotificador notificador)
-        {
-            _notificador = notificador;
-        }
+        //protected BaseController(INotificador notificador)
+        //{
+        //    _notificador = notificador;
+        //}
 
-        protected bool OperacaoValida()
-        {
-            return !_notificador.TemNotificacao();
-        }
+        //protected bool OperacaoValida()
+        //{
+        //    return !_notificador.TemNotificacao();
+        //}
     }
 }
