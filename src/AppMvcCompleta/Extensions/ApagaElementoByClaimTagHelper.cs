3845b5f21b8968112f1b18aa1541cf4cb4e8586e
renanflow <renanlopes.ads@gmail.com>
2022-02-04 08:08:18 -0300

    Taggelper ApagaElementoByActionTagHelper

--- src/AppMvcCompleta/Extensions/ApagaElementoByClaimTagHelper.cs
+++ src/AppMvcCompleta/Extensions/ApagaElementoByClaimTagHelper.cs
@@ -57,11 +57,8 @@ namespace DevIO.App.Extensions
             if (output == null)
                 throw new ArgumentNullException(nameof(output));
 
-            // Aqui no processamento ele vai fazer _contextAccessor.HttpContext.GetRouteData(). O GetRouteData vai pegar uma coleção de dados da rota através da chave "action", ou seja, eu quero saber o nome da "action".
-            // Então é assim que pegamos o nome de uma action dentro de um request
             var action = _contextAccessor.HttpContext.GetRouteData().Values["action"].ToString();
 
-            // Se a ActionName possui (contém) o valor da action da qual vc está chamando, ok, mata a operação aqui, caso não, faço o SuppressOutput, ou seja, ele não vai gerar o elemento
             if (ActionName.Contains(action)) return;
 
             output.SuppressOutput();
