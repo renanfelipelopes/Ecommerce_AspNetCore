227965ea528d0be6a2438ad56efbcc5c26979efb
renanflow <renanlopes.ads@gmail.com>
2022-02-08 22:02:57 -0300

    Remove ValidateAntiForgeryToken Fornecedor

--- src/AppMvcCompleta/Controllers/FornecedoresController.cs
+++ src/AppMvcCompleta/Controllers/FornecedoresController.cs
@@ -61,7 +61,6 @@ namespace DevIO.App.Controllers
         [ClaimsAuthorize("Fornecedor", "Adicionar")]
         [Route("novo-fornecedor")]
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(FornecedorViewModel fornecedorViewModel)
         {
             if (!ModelState.IsValid) return View(fornecedorViewModel);
@@ -90,7 +89,6 @@ namespace DevIO.App.Controllers
         [ClaimsAuthorize("Fornecedor", "Editar")]
         [Route("editar-fornecedor/{id:guid}")]
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Guid id, FornecedorViewModel fornecedorViewModel)
         {
             if (id != fornecedorViewModel.Id) return NotFound();
@@ -122,7 +120,6 @@ namespace DevIO.App.Controllers
         [ClaimsAuthorize("Fornecedor", "Excluir")]
         [Route("excluir-fornecedor/{id:guid}")]
         [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             var fornecedor = await ObterFornecedorEndereco(id);
